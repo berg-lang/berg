4ff85fabe2c8527ec261ce0b92a69edef0fa0bfd
John Keiser <john@johnkeiser.com>
2017-04-09 13:08:43 -0700

    Switch to using rspec for tests

--- ruby/berg_test.rb
+++
@@ -1,5 +0,0 @@
-require_relative "berg_test/test_run"
-
-test_path = File.expand_path("../../tests", __FILE__)
-test_run = BergTest::TestRun.new(test_path, whitelist: ARGV, output_stream: STDOUT)
-test_run.run
