fb0580981973e33aada9a71c627438bf27f1f9ed
John Keiser <john@johnkeiser.com>
2017-11-22 11:52:45 -0800

    Remove generic function indirection for readability

---
+++ rust/berg-compiler/src/parser/ast_builder.rs
@@ -0,0 +1,225 @@
+use compiler::Compiler;
+use compile_errors;
+use compiler::source_data::{ByteIndex,ByteRange,SourceIndex};
+use ast::AstIndex;
+use ast::token::{ExpressionBoundary,Fixity,InfixToken,Token};
+use ast::token::Token::*;
+use ast::token::ExpressionBoundary::*;
+use indexed_vec::{Delta,IndexedVec};
+
+#[derive(Debug)]
+pub(super) struct AstBuilder<'p,'c:'p> {
+    pub(super) compiler: &'p Compiler<'c>,
+    pub(super) source: SourceIndex,
+    tokens: IndexedVec<Token,AstIndex>,
+    token_ranges: IndexedVec<ByteRange,AstIndex>,
+    open_expressions: Vec<OpenExpression>,
+}
+
+#[derive(Debug)]
+struct OpenExpression {
+    open_index: AstIndex,
+    boundary: ExpressionBoundary,
+    infix: Option<(InfixToken,AstIndex)>,
+}
+
+impl<'p,'c:'p> AstBuilder<'p,'c> {
+    pub(super) fn new(compiler: &'p Compiler<'c>, source: SourceIndex) -> Self {
+        AstBuilder {
+            compiler,
+            source,
+            tokens: Default::default(),
+            token_ranges: Default::default(),
+            open_expressions: Default::default(),
+        }
+    }
+
+    pub(super) fn complete(self) -> (IndexedVec<Token,AstIndex>, IndexedVec<ByteRange,AstIndex>) {
+        assert!(self.open_expressions.is_empty());
+        (self.tokens, self.token_ranges)
+    }
+
+    pub(super) fn on_token(&mut self, token: Token, range: ByteRange) {
+        match token {
+            // Push the newly opened group onto open_expressions
+            Open(boundary, _) => self.on_open(boundary, range),
+            // Delay the close token so that we can see the next infix.
+            Close(boundary, _) => self.on_close(boundary, range),
+
+            // Infix tokens may have left->right or right->left precedence.
+            InfixOperator(_)|NewlineSequence|MissingInfix => {
+                // Open or close PrecedenceGroups as necessary based on this infix.
+                let infix = token.to_infix().unwrap();
+                self.handle_precedence(infix, range.start);
+                let infix_index = self.push(token, range);
+                // Set this as the last infix for future precedence checking
+                self.open_expressions.last_mut().unwrap().infix = Some((infix, infix_index));
+            },
+
+            _ => {
+                assert!(token.fixity() != Fixity::Infix);
+                self.push(token, range);
+            },
+        }
+    }
+
+    fn handle_precedence(&mut self, next_infix: InfixToken, next_infix_start: ByteIndex) {
+        if let Some((infix, index)) = self.open_expression().infix {
+            // The normal order of things is that infixes run left to right.
+            // If the next infix binds *tighter* than current, wrap it in a
+            // "invisible parentheses" (a precedence subexpression).
+            // e.g. 1+2*3 -> 1+2 -> 1+(2* ...
+            // e.g. 1*2>3+4 -> 1*2>(3+ ...
+            // e.g. 1+2>3*4 -> 1+2>(3* ...
+            // e.g. 1>2+3*4 -> 1>(2+(3* ...
+            if infix.takes_right_child(next_infix) {
+                let boundary = PrecedenceGroup;
+                let open_index = index+1;
+                self.open_expressions.push(OpenExpression { open_index, boundary, infix: None });
+
+            } else {
+                // If the current expression is precedence, and its *parent* doesn't
+                // want the next infix as a child, we have to close off the invisible
+                // parentheses. Repeat as necessary.
+                // 1+2*3>4 -> 1+(2*3) -> 1+(2*3)> ...
+                while self.open_expression().boundary == PrecedenceGroup {
+                    if let Some((parent_infix,_)) = self.parent_expression().infix {
+                        if !parent_infix.takes_right_child(next_infix) {
+                            self.close(next_infix_start..next_infix_start);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn open_expression(&self) -> &OpenExpression {
+        self.open_expressions.last().unwrap()
+    }
+
+    fn parent_expression(&self) -> &OpenExpression {
+        &self.open_expressions[self.open_expressions.len()-2]
+    }
+
+    fn on_close(&mut self, boundary: ExpressionBoundary, range: ByteRange) {
+        // Close lesser subexpressions: File > Parentheses > CompoundTerm > PrecedenceGroup
+        loop {
+            use std::cmp::Ordering::*;
+            let open_boundary = self.open_expression().boundary;
+            match boundary.partial_cmp(&open_boundary).unwrap() {
+                Greater => {
+                    // Close and continue. Report "open without close" if parentheses get closed early
+                    match open_boundary {
+                        Parentheses => {
+                            let source = self.source;
+                            let open_range = self.token_ranges[self.open_expression().open_index].clone();
+                            let close = String::from(")");
+                            self.compiler.report(compile_errors::OpenWithoutClose { source, open_range, close });
+                        },
+                        CompoundTerm|PrecedenceGroup|File => {},
+                    }
+                    self.close(range.start..range.start);
+                },
+                Equal => {
+                    self.close(range);
+                    break;
+                },
+                Less => {
+                    match boundary {
+                        Parentheses => {
+                            let source = self.source;
+                            let close_range = range.clone();
+                            let open = String::from("(");
+                            self.compiler.report(compile_errors::CloseWithoutOpen { source, close_range, open });
+                        },
+                        CompoundTerm|PrecedenceGroup|File => {},
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    fn close(&mut self, range: ByteRange) {
+        match self.open_expression().boundary {
+            File => self.actually_close(range),
+            PrecedenceGroup => {
+                // PrecedenceGroups are already known to be necessary by virtue of how we insert them.
+                self.actually_close(range);
+            },
+            Parentheses|CompoundTerm => {
+                // If we have an infix and there is a previous (parent) infix, the parentheses
+                // are necessary so we can be its right child.
+                // Otherwise, they are redundant (infix is processed left to right always).
+                if self.open_expression().infix.is_some() && self.parent_expression().infix.is_some() {
+                    self.actually_close(range);
+                } else {
+                    self.close_unnecessary(range);
+                }
+            },
+        }
+    }
+
+    fn actually_close(&mut self, range: ByteRange) {
+        let expression = self.open_expressions.pop().unwrap();
+        match expression.boundary {
+            File => {}, // Popping the expression is enough.
+            CompoundTerm|PrecedenceGroup => {
+                let start = self.token_ranges[expression.open_index].start;
+                let close_index = self.next_index()+1; // Have to add 1 due to the impending insert.
+                let delta = close_index-expression.open_index;
+                self.insert(expression.open_index, Open(expression.boundary, delta), start..start);
+                self.push(Close(expression.boundary, delta), range);
+            },
+            Parentheses => {
+                let mut delta = self.next_index()-expression.open_index;
+                // If we're fixing a missing open/close parentheses, we may be creating *empty* parens, which
+                // require a MissingExpression between them.
+                if delta == 1 { self.push(MissingExpression, range.start..range.start); delta = Delta(AstIndex(2)); }
+                if let Open(boundary, ref mut open_delta) = self.tokens[expression.open_index] {
+                    assert!(boundary == expression.boundary);
+                    *open_delta = delta;
+                } else {
+                    unreachable!()
+                }
+                self.push(Close(expression.boundary, delta), range);
+            },
+        }
+    }
+
+    fn close_unnecessary(&mut self, range: ByteRange) {
+        if self.open_expression().boundary == Parentheses {
+            self.report_unnecessary_parentheses(&range);
+            self.actually_close(range);
+        } else {
+            self.open_expressions.pop();
+        }
+    }
+
+    fn report_unnecessary_parentheses(&mut self, _range: &ByteRange) {
+        // TODO this is where you would warn!
+    }
+
+    fn on_open(&mut self, boundary: ExpressionBoundary, open_range: ByteRange) {
+        let open_index = self.next_index();
+        self.open_expressions.push(OpenExpression { open_index, boundary, infix: None });
+        match boundary {
+            Parentheses => { self.push(boundary.placeholder_open_token(), open_range); },
+            // CompoundTerm and PrecedenceGroup typically don't end up in the AST, so we don't insert
+            // them until we discover we have to.
+            CompoundTerm|PrecedenceGroup|File => {}
+        };
+    }
+
+    fn push(&mut self, token: Token, range: ByteRange) -> AstIndex {
+        self.tokens.push(token);
+        self.token_ranges.push(range)
+    }
+
+    fn insert(&mut self, index: AstIndex, token: Token, range: ByteRange) {
+        self.tokens.insert(index, token);
+        self.token_ranges.insert(index, range);
+    }
+
+    fn next_index(&self) -> AstIndex { self.tokens.len() }
+}
--- rust/berg-compiler/src/parser/tokenizer.rs
+++ rust/berg-compiler/src/parser/tokenizer.rs
@@ -1,35 +1,21 @@
+use compiler::source_data::ParseData;
 use ast::{IdentifierIndex,LiteralIndex};
 use ast::intern_pool::{StringPool,InternPool,Pool};
 use ast::identifiers;
 use ast::token::Token;
 use ast::token::Token::*;
 use ast::token::ExpressionBoundary::*;
-use compiler::Compiler;
-use compiler::source_data::{ByteIndex,ByteSlice,CharData,SourceIndex};
+use compiler::source_data::{ByteIndex,ByteSlice,CharData};
 use compiler::compile_errors;
-use compiler::source_data::{ByteRange};
+use parser::ast_builder::AstBuilder;
 use parser::scanner::Scanner;
 use parser::scanner::CharType::*;
 use parser::tokenizer::TokenizerState::*;
 use std::str;
 
-///
-/// Breaks a file into a series of Tokens, calling the given function for each
-/// token.
-/// 
-pub(super) fn tokenize<OnToken: FnMut(Token,ByteRange)->()>(
-    compiler: &Compiler,
-    source: SourceIndex,
-    on_token: OnToken
-) -> (CharData, StringPool<IdentifierIndex>, StringPool<LiteralIndex>) {
-    let tokenizer = Tokenizer::new(compiler, source);
-    tokenizer.tokenize(on_token)
-}
-
 #[derive(Debug)]
-struct Tokenizer<'p,'c:'p> {
-    compiler: &'p Compiler<'c>,
-    source: SourceIndex,
+pub(super) struct Tokenizer<'p,'c:'p> {
+    ast_builder: AstBuilder<'p,'c>,
     char_data: CharData,
     identifiers: InternPool<IdentifierIndex>,
     literals: StringPool<LiteralIndex>,
@@ -46,34 +32,36 @@ enum TokenizerState {
 }
 
 impl<'p,'c:'p> Tokenizer<'p,'c> {
-    fn new(compiler: &'p Compiler<'c>, source: SourceIndex) -> Self {
-        let char_data = Default::default();
-        let identifiers = identifiers::intern_all();
-        let literals = Default::default();
-        Tokenizer { compiler, source, char_data, identifiers, literals }
+    pub(super) fn new(ast_builder: AstBuilder<'p,'c>) -> Self {
+        Tokenizer {
+            ast_builder,
+            char_data: Default::default(),
+            identifiers: identifiers::intern_all(),
+            literals: Default::default(),
+        }
     }
 
-    fn tokenize<OnToken: FnMut(Token,ByteRange)->()>(mut self, mut on_token: OnToken) -> (CharData, StringPool<IdentifierIndex>, StringPool<LiteralIndex>) {
-        let buffer = self.compiler.with_source(self.source, |source_data| {
+    pub(super) fn tokenize(&mut self) {
+        let buffer = self.ast_builder.compiler.with_source(self.ast_builder.source, |source_data| {
             let source_spec = source_data.source_spec();
-            source_spec.open(self.compiler, self.source)
+            source_spec.open(self.ast_builder.compiler, self.ast_builder.source)
         });
 
         let mut scanner = Scanner::default();
         let mut start = scanner.index;
         let mut state = TokenizerState::Start;
 
-        Self::emit_token(File.placeholder_open_token(), state, &buffer, start, &scanner, &mut on_token);
+        self.emit_token(File.placeholder_open_token(), state, &buffer, start, &scanner);
 
         while let Some(char_type) = scanner.next(&buffer) {
             use parser::scanner::CharType::*;
             state = match char_type {
-                Digit       => self.integer(state, &buffer, start, &mut scanner, &mut on_token),
-                Identifier  => self.identifier(state, &buffer, start, &mut scanner, &mut on_token),
-                Operator    => self.operator(state, &buffer, start, &mut scanner, &mut on_token),
-                Separator   => self.separator(state, &buffer, start, &mut scanner, &mut on_token),
-                Open        => Self::emit_token(Parentheses.placeholder_open_token(), state, &buffer, start, &scanner, &mut on_token),
-                Close       => Self::emit_token(Parentheses.placeholder_close_token(), state, &buffer, start, &scanner, &mut on_token),
+                Digit       => self.integer(state, &buffer, start, &mut scanner),
+                Identifier  => self.identifier(state, &buffer, start, &mut scanner),
+                Operator    => self.operator(state, &buffer, start, &mut scanner),
+                Separator   => self.separator(state, &buffer, start, &mut scanner),
+                Open        => self.emit_token(Parentheses.placeholder_open_token(), state, &buffer, start, &scanner),
+                Close       => self.emit_token(Parentheses.placeholder_close_token(), state, &buffer, start, &scanner),
                 Newline     => { self.char_data.append_line(scanner.index); state.after_newline(start, scanner.index) },
                 Space       => { scanner.next_while(Space, &buffer); state.after_space() },
                 Unsupported => self.report_unsupported(state, &buffer, start, &mut scanner),
@@ -83,95 +71,101 @@ impl<'p,'c:'p> Tokenizer<'p,'c> {
             start = scanner.index;
         }
 
-        Self::emit_token(File.placeholder_close_token(), state, &buffer, start, &scanner, &mut on_token);
-
-        (self.char_data, self.identifiers.strings, self.literals)
+        self.emit_token(File.placeholder_close_token(), state, &buffer, start, &scanner);
     }
 
-    fn emit_token<OnToken: FnMut(Token,ByteRange)->()>(
+    pub(super) fn complete(self) -> ParseData {
+        let (tokens, token_ranges) = self.ast_builder.complete();
+        ParseData {
+            char_data: self.char_data,
+            identifiers: self.identifiers.strings,
+            literals: self.literals,
+            tokens: tokens,
+            token_ranges: token_ranges,
+        }
+    }
+
+    fn emit_token(
+        &mut self,
         token: Token,
         state: TokenizerState,
         buffer: &ByteSlice,
         start: ByteIndex,
-        scanner: &Scanner,
-        on_token: &mut OnToken
+        scanner: &Scanner
     ) -> TokenizerState {
         if token.has_left_operand() {
             // Insert missing expression if applicable
             if !state.is_left_operand() {
-                on_token(MissingExpression, start..start)
+                self.ast_builder.on_token(MissingExpression, start..start)
             }
         } else {
             // Insert newline sequence operator if applicable
             if let NewlineAfterOperand { newline_start, newline_length } = state {
                 let newline_end = newline_start+(newline_length as usize);
-                on_token(NewlineSequence, newline_start..newline_end);
+                self.ast_builder.on_token(NewlineSequence, newline_start..newline_end);
 
             // Insert missing infix if applicable
             } else if state.is_left_operand() {
-                on_token(MissingInfix, start..start)
+                self.ast_builder.on_token(MissingInfix, start..start)
             }
 
             // Insert open compound term if applicable
             if state.is_space() {
-                on_token(CompoundTerm.placeholder_open_token(), start..start);
+                self.ast_builder.on_token(CompoundTerm.placeholder_open_token(), start..start);
             }
         }
 
-        on_token(token, start..scanner.index);
+        self.ast_builder.on_token(token, start..scanner.index);
 
         if token.has_right_operand() {
             TokenizerState::ImmediateLeftOperator
         } else {
             // Insert close compound term if applicable
             if scanner.peek_if_space(buffer) {
-                on_token(CompoundTerm.placeholder_close_token(), scanner.index..scanner.index);
+                self.ast_builder.on_token(CompoundTerm.placeholder_close_token(), scanner.index..scanner.index);
             }
             TokenizerState::ImmediateLeftOperand
         }
     }
 
-    fn integer<OnToken: FnMut(Token,ByteRange)->()>(
+    fn integer(
         &mut self,
         state: TokenizerState,
         buffer: &ByteSlice,
         start: ByteIndex,
-        scanner: &mut Scanner,
-        on_token: &mut OnToken
+        scanner: &mut Scanner
     ) -> TokenizerState {
         scanner.next_while(Digit, buffer);
         let token_type = if scanner.next_while_identifier(buffer) {
-            self.compiler.report(compile_errors::IdentifierStartsWithNumber { source: self.source, identifier: start..scanner.index });
+            self.ast_builder.compiler.report(compile_errors::IdentifierStartsWithNumber { source: self.ast_builder.source, identifier: start..scanner.index });
             SyntaxErrorTerm
         } else {
             IntegerLiteral
         };
         let string = unsafe { str::from_utf8_unchecked(&buffer[start..scanner.index]) };
         let literal = self.literals.add(string);
-        Self::emit_token(token_type(literal), state, buffer, start, scanner, on_token)
+        self.emit_token(token_type(literal), state, buffer, start, scanner)
     }
 
-    fn identifier<OnToken: FnMut(Token,ByteRange)->()>(
+    fn identifier(
         &mut self,
         state: TokenizerState,
         buffer: &ByteSlice,
         start: ByteIndex,
-        scanner: &mut Scanner,
-        on_token: &mut OnToken
+        scanner: &mut Scanner
     ) -> TokenizerState {
         scanner.next_while_identifier(buffer);
         let string = unsafe { str::from_utf8_unchecked(&buffer[start..scanner.index]) };
         let identifier = self.identifiers.add(string);
-        Self::emit_token(PropertyReference(identifier), state, buffer, start, scanner, on_token)
+        self.emit_token(PropertyReference(identifier), state, buffer, start, scanner)
     }
 
-    fn operator<OnToken: FnMut(Token,ByteRange)->()>(
+    fn operator(
         &mut self,
         state: TokenizerState,
         buffer: &ByteSlice,
         start: ByteIndex,
-        scanner: &mut Scanner,
-        on_token: &mut OnToken
+        scanner: &mut Scanner
     ) -> TokenizerState {
         scanner.next_while(Operator, buffer);
         let string = unsafe { str::from_utf8_unchecked(&buffer[start..scanner.index]) };
@@ -185,20 +179,19 @@ impl<'p,'c:'p> Tokenizer<'p,'c> {
         } else {
             InfixOperator(identifier)
         };
-        Self::emit_token(token, state, buffer, start, scanner, on_token)
+        self.emit_token(token, state, buffer, start, scanner)
     }
 
-    fn separator<OnToken: FnMut(Token,ByteRange)->()>(
+    fn separator(
         &mut self,
         state: TokenizerState,
         buffer: &ByteSlice,
         start: ByteIndex,
-        scanner: &mut Scanner,
-        on_token: &mut OnToken
+        scanner: &mut Scanner
     ) -> TokenizerState {
         let string = unsafe { str::from_utf8_unchecked(&buffer[start..scanner.index]) };
         let identifier = self.identifiers.add(string);
-        Self::emit_token(InfixOperator(identifier), state, buffer, start, scanner, on_token)
+        self.emit_token(InfixOperator(identifier), state, buffer, start, scanner)
     }
 
     fn report_unsupported(
@@ -209,7 +202,7 @@ impl<'p,'c:'p> Tokenizer<'p,'c> {
         scanner: &mut Scanner
     ) -> TokenizerState {
         scanner.next_while(Unsupported, buffer);
-        self.compiler.report(compile_errors::UnsupportedCharacters { source: self.source, characters: start..scanner.index });
+        self.ast_builder.compiler.report(compile_errors::UnsupportedCharacters { source: self.ast_builder.source, characters: start..scanner.index });
         state.after_space()
     }
  
@@ -221,7 +214,7 @@ impl<'p,'c:'p> Tokenizer<'p,'c> {
         scanner: &mut Scanner
     ) -> TokenizerState {
         scanner.next_while(InvalidUtf8, buffer);
-        self.compiler.report(compile_errors::InvalidUtf8 { source: self.source, bytes: start..scanner.index });
+        self.ast_builder.compiler.report(compile_errors::InvalidUtf8 { source: self.ast_builder.source, bytes: start..scanner.index });
         state.after_space()
     }
 }
