b72f43782798f62035eb184221652ad2ab02cc1b
John Keiser <john@johnkeiser.com>
2017-12-10 15:20:12 -0800

    Add scope tests

---
+++ rust/berg-compiler/tests/scope.rs
@@ -0,0 +1,13 @@
+#[macro_use]
+pub mod compiler_test;
+
+compiler_tests! {
+    child_references_parent: "a = 1; { a + 2 }" => type(3),
+    child_references_parent_too_early_error: "{ a + 2 }; a = 1" => error(NoSuchProperty@2) type(nothing),
+    child_references_parent_container_error: "a = { a + 1 }" => error(NoSuchProperty@6) type(nothing),
+    child_defines_value_early: "x = { a = 1; a+2 }; a = x + 3; a" => type(6),
+    child_updates_parent: "a = 1; { a += 2 }; a + 3" => type(6),
+    child_overwrites_parent: "a = 1; { a = 2 }; a + 3" => type(5),
+    child_overwrites_and_references_parent: "a = 1; { a = 2; a + 3 } + a" => type(7),
+    parent_references_child_error: "{ a = 0; a }; a" => error(NoSuchProperty@14) type(error),
+}
